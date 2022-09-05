06372edb8fb64c4d770fe051984ce121fca2cff9
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-09-06 02:43:25 +0900

    Add files via upload

--- x.py
+++ x.py
@@ -2,12 +2,13 @@
 from tkinter import *
 from tkinter import filedialog as tkfd
 from tkinter import messagebox as tkmb
+from tkinter import ttk
 from datetime import *
 
 #ウィンドウ
 w = Tk()
 w.title("タイトル")
-w.geometry("800x400")
+w.geometry("1000x700")
 
 #メニューバー
 mb = Menu(w)
@@ -91,22 +92,46 @@ sb.grid(column=0, row=8)
 sb_b = Button(lf, text="選択内容表示", command=lambda: print(varspinbox.get()))
 sb_b.grid(column=1, row=8)
 
+#スケール
+scale_var = IntVar()
+s = Scale(lf, variable=scale_var, orient=HORIZONTAL, length=300, from_=0, to=100, tickinterval=10)
+s.grid(column=0, row=9)
+
+#ノートブック
+
 # ファイル
 def fd():
     fp = tkfd.askopenfilename(initialdir="C:", filetypes=[("Pythonファイル", "*.py")])
     print(fp)
 fd_b = Button(lf, text="ファイル選択", command=fd)
-fd_b.grid(column=0, row=9)
+fd_b.grid(column=0, row=10)
 
 #ダイアログ
 def d():
     tkmb.showinfo("ダイアログ", "内容")
 d_b = Button(lf, text="ダイアログ表示", command=d)
-d_b.grid(column=0, row=10)
+d_b.grid(column=0, row=11)
+
+# 表
+col_num = [1, 2]
+x = [(1, "太郎"), (2, "次郎")]
+tv = ttk.Treeview(lf, columns=col_num, height=len(x))
+
+tv.column("#0", width=0, stretch="no")
+tv.column(1, width=50)
+tv.column(2, width=100)
+
+tv.heading("#0", text="")
+tv.heading(1, text="ID", anchor="center")
+tv.heading(2, text="名前", anchor="center")
+
+for i in range(len(x)):
+    if i % 2 == 0:
+        tv.insert("", "end", values=x[i])
+    else:
+        tv.insert("", "end", values=x[i], tags="odd")
+        tv.tag_configure("odd", background="aqua")
+
+tv.grid(column=0, row=12)
 
 w.mainloop()
-
-#スケール
-#ツリービュー
-#プログレスバー
-#ノートブック
